41aba788cbf40775acfe59e94c62206af08693ff
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2022-09-01 23:45:26 +0900

    finished works section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { AiFIllEye, AiFillGithub } from "react-icons/ai";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 
 import { AppWrap } from "../../wrapper";
@@ -19,7 +19,20 @@ const Work = () => {
 			setFilterWork(data);
 		});
 	});
-	const handleWorkFilter = (item) => {};
+	const handleWorkFilter = (item) => {
+		setActiveFilter(item);
+		setAnimateCard([{ y: 100, opacity: 0 }]);
+
+		setTimeout(() => {
+			setAnimateCard([{ y: 0, opacity: 1 }]);
+
+			if (item === "All") {
+				setFilterWork(works);
+			} else {
+				setFilterWork(works.filter((work) => work.tags.includes(item)));
+			}
+		}, 500);
+	};
 	return (
 		<>
 			<h2 className="head-text">
@@ -53,6 +66,55 @@ const Work = () => {
 					<div className="app__work-item app__flex" key={index}>
 						<div className="app__work-img app__flex">
 							<img src={urlFor(work.imgUrl)} alt={work.name} />
+
+							<motion.div
+								whileHover={{ opacity: [0, 1] }}
+								transition={{
+									durtion: 0.25,
+									ease: "easeInOut",
+									staggerChildren: 0,
+								}}
+								className="app__work-hover app__flex"
+							>
+								<a
+									href={work.projectLink}
+									target="_blank"
+									rel="norefer"
+								>
+									<motion.div
+										whileInView={{ scale: [0, 1] }}
+										whileHover={{ scale: [0, 0.9] }}
+										transition={{ durtion: 0.25 }}
+										className="app__flex"
+									>
+										<AiFillEye />
+									</motion.div>
+								</a>
+								<a
+									href={work.codeLink}
+									target="_blank"
+									rel="norefer"
+								>
+									<motion.div
+										whileInView={{ scale: [0, 1] }}
+										whileHover={{ scale: [0, 0.9] }}
+										transition={{ durtion: 0.25 }}
+										className="app__flex"
+									>
+										<AiFillGithub />
+									</motion.div>
+								</a>
+							</motion.div>
+						</div>
+						<div className="app__work-content app__flex">
+							<h4 className="bold-text">{work.title}</h4>
+							<p className="p-text" style={{ marginTop: 10 }}>
+								{work.description}
+							</p>
+
+							<div className="app__work-tag app__flex">
+								<p className="p-text">{work.tags[0]}</p>
+							</div>
 						</div>
 					</div>
 				))}
@@ -61,4 +123,4 @@ const Work = () => {
 	);
 };
 
-export default Work;
+export default AppWrap(Work, "app__works");
