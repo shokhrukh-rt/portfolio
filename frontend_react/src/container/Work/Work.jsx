12530802e02c6f30b7fdd13de77109cf7407f351
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2022-09-06 02:32:52 +0900

    Finished Skills&Xperiences

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Work.scss";
 
@@ -62,7 +62,7 @@ const Work = () => {
 				transition={{ duration: 0.5, delayChildren: 0.5 }}
 				className="app__work-portfolio"
 			>
-				{filterWork.map((work, index) => (
+				{filterWork?.map((work, index) => (
 					<div className="app__work-item app__flex" key={index}>
 						<div className="app__work-img app__flex">
 							<img src={urlFor(work.imgUrl)} alt={work.name} />
@@ -72,14 +72,14 @@ const Work = () => {
 								transition={{
 									durtion: 0.25,
 									ease: "easeInOut",
-									staggerChildren: 0,
+									staggerChildren: 0.5,
 								}}
 								className="app__work-hover app__flex"
 							>
 								<a
 									href={work.projectLink}
 									target="_blank"
-									rel="norefer"
+									rel="noreferrer"
 								>
 									<motion.div
 										whileInView={{ scale: [0, 1] }}
@@ -93,7 +93,7 @@ const Work = () => {
 								<a
 									href={work.codeLink}
 									target="_blank"
-									rel="norefer"
+									rel="noreferrer"
 								>
 									<motion.div
 										whileInView={{ scale: [0, 1] }}
@@ -123,4 +123,8 @@ const Work = () => {
 	);
 };
 
-export default AppWrap(Work, "app__works");
+export default AppWrap(
+	MotionWrap(Work, "app__works"),
+	"works",
+	"app__primarybg"
+);
