a81269c4f0ead61c76ed80023660f9740169ddb6
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2022-09-26 23:39:43 +0900

    small changes

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -61,7 +61,7 @@ const Header = () => {
 				whileInView={scaleVariants.whileInView}
 				className="app__header-circles"
 			>
-				{[images.flutter, images.redux, images.sass].map(
+				{[images.node, images.redux, images.sass].map(
 					(circle, index) => (
 						<div
 							className="circle-cmp app__flex"
